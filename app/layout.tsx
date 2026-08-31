6ed1627e18833243f9ec3fab213d2bd9d8c60304
Bomseed udotani <paramparuay1989@gmail.com>
2026-09-01 04:56:02 +0700

    Modify import for AppProvider in layout.tsx
    Updated import statement for AppProvider from context.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
+// 🔴 แก้ไขบรรทัดนี้ ให้ถอยออกไปหาโฟลเดอร์ context
+import { AppProvider } from "../context/AppContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
